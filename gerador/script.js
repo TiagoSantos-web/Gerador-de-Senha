abc840b8231ea4cd4e591d8f8ffcb30f0c17608c
unknown <studiocar8@gmail.com>
2025-10-20 22:31:10 -0300

    Atualização do projeto: novas funcionalidades e melhorias

--- gerador/script.js
+++ gerador/script.js
@@ -1,34 +1,35 @@
-let sliderElement = document.querySelector("#slider");
-let buttonElement = document.querySelector("#button");
-let sizePassword = document.querySelector("#valor");
-let password = document.querySelector("#password");
-let containerPassword = document.querySelector("#container-password");
 
-let charset = "abcdefghijklmopqrstuvxwyzABCDEFGHIJKLMNOPQRSTUVXWYZ123456789";
-let novasenha = "";
+const sliderElement = document.querySelector("#slider");
+const buttonElement = document.querySelector("#button");
+const sizePassword = document.querySelector("#valor");
+const password = document.querySelector("#password");
+const containerPassword = document.querySelector("#container-password");
 
-sizePassword.innerHTML = sliderElement.value;
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*?";
 
-slider.oninput = function(){
-    sizePassword.innerHTML = this.value;
+
+sizePassword.textContent = sliderElement.value;
+sliderElement.addEventListener("input", () => {
+  sizePassword.textContent = sliderElement.value;
+});
+
+
+function generatePassword() {
+  let pass = "";
+  const length = sliderElement.value;
+
+  for (let i = 0; i < length; i++) {
+    pass += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+
+  password.textContent = pass;
+  containerPassword.classList.remove("hide");
 }
 
 
-function generatePassword(){
-  
-    let pass ="";
-   
-    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
-        pass += charset.charAt(math.floor(math.random() * n));
-    }
-    containerPassword.classList.remove("hide");
-  password.innerHTML = pass;
-
-}
-
-function copyPassword(){
-    alert("senha copiada com sucesso!")
-    navigator.clipboard.writeText(novasenha);
-}
-
-
+password.addEventListener("click", () => {
+  if (password.textContent) {
+    navigator.clipboard.writeText(password.textContent);
+    alert("Senha copiada com sucesso! ✅");
+  }
+});
